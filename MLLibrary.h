41ad88bb1bc7fb1d343c86dcc39f3b6549ed394d
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2024-01-01 23:24:29 -0500

    Consolidate ML Library

---
+++ MLLibrary.h
@@ -0,0 +1,16 @@
+#ifndef MLLIBRARY_H
+#define MLLIBRARY_H
+
+#include "LinearRegression.h"
+#include "LogisticRegression.h"
+#include "DecisionTree.h"
+#include "KMeans.h"
+#include "DataSplit.h"
+#include "NeuralNetwork.h"
+#include "DataNormalization.h"
+#include "DataStandardization.h"
+#include "MonteCarlo.h"
+#include "LossFunction.h"
+#include "LossFunction.c"
+
+#endif // MLLIBRARY_H
